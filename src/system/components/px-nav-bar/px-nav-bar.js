ea12f261c221981a26a2dd1152b968c3b879bda8
Weslei Freitas <weslei.rfreitas@gmail.com>
2015-08-27 09:16:09 -0300

    fix(px-nav-bar): remover JSHint Problems

--- src/system/components/px-nav-bar/px-nav-bar.js
+++ src/system/components/px-nav-bar/px-nav-bar.js
@@ -1,25 +1,25 @@
-/**
- * Module pxNavBar
- */
-angular.module('pxNavBar', [])
-    .value('pxNavBarConfig', {
+(function () {
+    'use strict';
 
-    })
-    .directive('pxNavBar', ['pxNavBarConfig', 'pxConfig', function(pxDataGridConfig, pxConfig) {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: false,
-            templateUrl: pxConfig.PX_PACKAGE + 'system/components/px-nav-bar/px-nav-bar.cfm',
-            link: function(scope, element, attrs) {
 
-                scope.logo = pxConfig.PX_PACKAGE + 'system/assets/richsolutions/richsolutions_bola_200x200.jpg';
+    angular.module('pxNavBar', [])
+        .value('pxNavBarConfig', {
+
+        })
+        .directive('pxNavBar', ['pxNavBarConfig', 'pxConfig', function (pxDataGridConfig, pxConfig) {
+            return {
+                restrict: 'E',
+                replace: true,
+                transclude: false,
+                templateUrl: pxConfig.PX_PACKAGE + 'system/components/px-nav-bar/px-nav-bar.cfm',
+                link: function (scope, element, attrs) {
+
+                    scope.logo = pxConfig.PX_PACKAGE + 'system/assets/richsolutions/richsolutions_bola_200x200.jpg';
+                }
             }
-        }
     }])
-    .controller('pxNavBarCtrl', ['pxConfig', '$scope', '$http', function(pxConfig, $scope, $http) {
-        $scope.templates =
-            [{
+        .controller('pxNavBarCtrl', ['pxConfig', '$scope', '$http', function (pxConfig, $scope, $http) {
+            $scope.templates = [{
                 name: '?.html',
                 url: pxConfig.PX_PACKAGE + '?.html'
             }, {
@@ -27,40 +27,41 @@ angular.module('pxNavBar', [])
                 url: ''
             }];
 
-        $scope.template = $scope.templates[1];
+            $scope.template = $scope.templates[1];
 
-        //console.log($scope.template);
+            //console.log($scope.template);
 
-        $scope.showView = function(view) {
+            $scope.showView = function (view) {
 
-            var params = new Object();
-            params.com_id = view;
-            params.pathname = document.location.pathname;
-            params.pxProjectPackage = pxConfig.PX_PACKAGE;
+                var params = new Object();
+                params.com_id = view;
+                params.pathname = document.location.pathname;
+                params.pxProjectPackage = pxConfig.PX_PACKAGE;
 
-            $http({
-                method: 'POST',
-                url: pxConfig.PX_PACKAGE + 'system/components/px-nav-bar/px-nav-bar.cfc?method=getView',
-                params: params
-            }).success(function(result) {
+                $http({
+                    method: 'POST',
+                    url: pxConfig.PX_PACKAGE + 'system/components/px-nav-bar/px-nav-bar.cfc?method=getView',
+                    params: params
+                }).success(function (result) {
 
-                var headerView = result.qView[0].MEN_NOMECAMINHO.split(result.qView[0].MEN_NOMECAMINHO.split('»')[result.qView[0].MEN_NOMECAMINHO.split('»').length - 1])
+                    var headerView = result.qView[0].MEN_NOMECAMINHO.split(result.qView[0].MEN_NOMECAMINHO.split('»')[result.qView[0].MEN_NOMECAMINHO.split('»').length - 1])
 
-                $scope.view = new Object();
-                $scope.view.result = result;
-                $scope.view.men_id = result.qView[0].MEN_ID;
-                $scope.view.caminho = result.qView[0].MEN_NOMECAMINHO;
-                $scope.view.header = headerView[0];
-                $scope.view.titulo = result.qView[0].MEN_NOMECAMINHO.split('»')[result.qView[0].MEN_NOMECAMINHO.split('»').length - 1];
-                $scope.view.icon = result.qView[0].COM_ICON;
+                    $scope.view = new Object();
+                    $scope.view.result = result;
+                    $scope.view.men_id = result.qView[0].MEN_ID;
+                    $scope.view.caminho = result.qView[0].MEN_NOMECAMINHO;
+                    $scope.view.header = headerView[0];
+                    $scope.view.titulo = result.qView[0].MEN_NOMECAMINHO.split('»')[result.qView[0].MEN_NOMECAMINHO.split('»').length - 1];
+                    $scope.view.icon = result.qView[0].COM_ICON;
 
-                $scope.templates[1].name = result.qView[0].COM_VIEW;
-                $scope.templates[1].url = result.qView[0].COM_VIEW;
+                    $scope.templates[1].name = result.qView[0].COM_VIEW;
+                    $scope.templates[1].url = result.qView[0].COM_VIEW;
 
-            }).
-            error(function(data, status, headers, config) {
-                // Erro
-                alert('Ops! Ocorreu um erro inesperado.\nPor favor contate o administrador do sistema!');
-            });
-        }
+                }).
+                error(function (data, status, headers, config) {
+                    // Erro
+                    alert('Ops! Ocorreu um erro inesperado.\nPor favor contate o administrador do sistema!');
+                });
+            }
     }]);
+})();
