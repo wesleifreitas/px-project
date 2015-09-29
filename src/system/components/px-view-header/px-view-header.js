65bb579e7f94d1e332ff800dd5a10164e93d3ec4
Weslei Freitas <weslei.rfreitas@gmail.com>
2015-09-29 11:47:42 -0300

    style(px-view-header): ajustar código

--- src/system/components/px-view-header/px-view-header.js
+++ src/system/components/px-view-header/px-view-header.js
@@ -1,23 +1,23 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('pxViewHeader', [])
         .value('pxViewHeaderConfig', {
 
         })
-        .directive('pxViewHeader', ['pxViewHeaderConfig', 'pxConfig', function (pxViewHeaderConfig, pxConfig) {
+        .directive('pxViewHeader', ['pxViewHeaderConfig', 'pxConfig', function(pxViewHeaderConfig, pxConfig) {
             return {
                 restrict: 'E',
                 replace: true,
                 transclude: false,
                 templateUrl: pxConfig.PX_PACKAGE + 'system/components/px-view-header/px-view-header.html',
 
-                link: function (scope, element, attrs) {
+                link: function(scope, element, attrs) {
                     // Manipulação e Eventos DOM
                 },
-                controller: function ($scope, $element, $attrs) {
+                controller: function($scope, $element, $attrs) {
                     // Controller
                 }
             };
-    }]);
+        }]);
 })();
