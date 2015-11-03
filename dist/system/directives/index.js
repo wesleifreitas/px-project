fb67a5270cac964ecb6879db107d6e29d8371013
Weslei Freitas <weslei.rfreitas@gmail.com>
2015-11-03 09:46:26 -0200

    chore(dist): atualizar dist

--- dist/system/directives/index.js
+++ dist/system/directives/index.js
@@ -1,1 +1,1 @@
-define(["../components/px-nav-bar/px-nav-bar","../components/px-container/px-container","../components/px-view-header/px-view-header","../components/px-data-grid/px-data-grid","../components/px-form-item/px-form-item","../components/px-form/px-form"],function(){});
+define(["../components/px-nav-bar/px-nav-bar","../components/px-container/px-container","../components/px-view-header/px-view-header","../components/px-data-grid/px-data-grid","../components/px-form-item/px-form-item"],function(){});
