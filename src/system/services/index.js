e11d7ce80c43502c8455b6ead524f6f33ec240ac
Weslei Freitas <weslei.rfreitas@gmail.com>
2015-12-02 16:12:08 -0200

    feat(px-string-util): criar serviço pxStringUtil

--- src/system/services/index.js
+++ src/system/services/index.js
@@ -2,6 +2,7 @@ define([
 	'../core/px-css-loader',
 	'../utils/js/px-util',
 	'../utils/js/px-array-util',
+	'../utils/js/px-string-util',
 	'../utils/js/px-mask-util',
 	'../components/px-data-grid/px-data-grid.service',
 	'../components/px-form/px-form.service',
