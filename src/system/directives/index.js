57be2e352e8e395ff29611b1bcdc932dedd0e676
Weslei Freitas <weslei.rfreitas@gmail.com>
2015-12-04 13:20:20 -0200

    feat(px-group-search): adicionar directive pxGroupSearch

--- src/system/directives/index.js
+++ src/system/directives/index.js
@@ -4,5 +4,6 @@ define([
 	'../components/px-view-header/px-view-header',
 	'../components/px-data-grid/px-data-grid',
 	'../components/px-form-item/px-form-item',
-	'../components/px-form/px-form'
+	'../components/px-form/px-form',
+	'../components/px-group-search/px-group-search'
 ], function() {});
