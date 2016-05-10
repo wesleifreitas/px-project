b36d788d40c8d05225ebd36234bbf5b1cae152a5
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-05-10 16:02:41 -0300

    feat(px-date-util): criar pxDateUtil
    Criar px-date-util.
    Função dateAdd:
    - yyyy: Year: 0%
    - q: Quarter: 0%
    - m: Month: 0%
    - y: Day of year: 0%
    - d: Day: 100%
    - w: Weekday: 0%
    - ww: Week: 0%
    - h: Hour: 0%
    - n: Minute: 0%
    - s: Second: 0%
    - l: Millisecond: 0%

--- src/system/services/index.js
+++ src/system/services/index.js
@@ -4,6 +4,7 @@ define([
 	'../utils/js/px-array-util',
 	'../utils/js/px-string-util',
 	'../utils/js/px-mask-util',
+	'../utils/js/px-date-util',
 	'../components/px-data-grid/px-data-grid.service',
 	'../components/px-form/px-form.service',
 	'../perfil/perfil.service',
